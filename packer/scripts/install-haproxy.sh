1ff07f5c9e3c14387598fb516837f8e87949599b
akatsantonis <aggelos.katsantonis@hotmail.com>
2025-04-29 17:15:03 +0300

    packer:haproxy-node: Change kernel settings

--- packer/scripts/install-haproxy.sh
+++ packer/scripts/install-haproxy.sh
@@ -2,6 +2,11 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Modify Kernel settings to to allow
+# more open file descriptors
+echo "fs.nr_open = 1048599" | sudo tee -a /etc/sysctl.d/98-haproxy.conf
+sysctl --system
+
 echo "[+] Installing HAProxy"
 apt-get install -y haproxy
 systemctl stop haproxy
