1e9b436ead9ebe4614d76038fabd892cd8f704a9
Angelos Katsantonis <38293432+akatsantonis@users.noreply.github.com>
2025-05-10 21:36:31 +0300

    github:workflows: Make a clone of the repo and then tag (#60)

--- scripts/tag-release.sh
+++ scripts/tag-release.sh
@@ -7,10 +7,18 @@ CICD_TOKEN="${CICD_TOKEN:-}"
 ENVIRONMENT="${ENVIRONMENT:-testing}"
 RELEASE="${RELEASE:-0.0.1-testing}"
 GITHUB_REPOSITORY="${GITHUB_REPOSITORY:-}"
+REPO_URL="https://github.com/$GITHUB_REPOSITORY.git"
+WORK_DIR=$(mktemp -d)
 
+cd "$WORK_DIR"
+# Configure git
 git config user.name "github-actions[bot]"
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git remote set-url origin "https://x-access-token:$CICD_TOKEN@github.com/$GITHUB_REPOSITORY"
 
+# Clone
+git clone "https://${CICD_TOKEN}@${REPO_URL#https://}" repo
+cd repo
+
+git remote set-url origin "https://x-access-token:$CICD_TOKEN@github.com/$GITHUB_REPOSITORY"
 git tag -a "v$RELEASE" -m "Release $RELEASE for $ENVIRONMENT environment"
 git push origin "v$RELEASE"
