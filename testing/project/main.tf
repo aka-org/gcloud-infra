ccd0bb4fddd115a8f2174bd3741f3441bbf54781
Angelos Katsantonis <aggelos.katsantonis@hotmail.com>
2025-05-23 02:12:00 +0300

    Fix missing varibale in project and export additional env variables during provisioning

--- testing/project/main.tf
+++ testing/project/main.tf
@@ -12,12 +12,12 @@ module "project" {
   version = "1.2.0"
 
   project_name       = var.project_name
-  project_labels     = var.project_labels
+  project_labels     = local.project_labels
   billing_account_id = var.billing_account_id
   enable_apis        = var.enable_apis
   sa_roles           = var.sa_roles
   bucket_name_prefix = var.bucket_name_prefix
-  bucket_labels      = var.bucket_labels
+  bucket_labels      = local.bucket_labels
   gcs_backend        = var.gcs_backend
   gha_wif_enabled    = var.gha_wif_enabled
   gha_owner_id       = var.gha_owner_id
